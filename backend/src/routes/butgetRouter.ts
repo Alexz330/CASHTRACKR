3c216b72c6974b04489d09fbffec0f2706523dd5
Alexz <alexislc330@gmail.com>
2025-07-18 14:33:21 -0600

    feat(routes): agregar ruta GET para obtener todos los budgets

---
+++ backend/src/routes/butgetRouter.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { BudgetController } from "../controllers/BudgetController";
+const router = Router();
+
+router.get("/", BudgetController.getAll);
+
+export default router;
