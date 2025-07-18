cfc853f7f2b5807cad3950414a80762b4359b5bc
Alexz <alexislc330@gmail.com>
2025-07-18 14:41:21 -0600

    feat(controller): agregar métodos CRUD básicos en BudgetController

--- backend/src/controllers/BudgetController.ts
+++ backend/src/controllers/BudgetController.ts
@@ -18,4 +18,31 @@ export class BudgetController {
       res.status(500).json({ message: "Error al crear el presupuesto" });
     }
   };
+
+  static getById = async (req: Request, res: Response) => {
+    try {
+      res.json({ message: "Budget by id" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Error al obtener el presupuesto" });
+    }
+  };
+
+  static updateById = async (req: Request, res: Response) => {
+    try {
+      res.json({ message: "Budget updated by id" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Error al actualizar el presupuesto" });
+    }
+  };
+
+  static deleteById = async (req: Request, res: Response) => {
+    try {
+      res.json({ message: "Budget deleted by id" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Error al eliminar el presupuesto" });
+    }
+  };
 }
