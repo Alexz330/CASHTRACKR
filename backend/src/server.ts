a9ae4f094584026e5565961435343c92cfe8415e
Alexz <alexislc330@gmail.com>
2025-07-18 14:34:31 -0600

    feat(app): integrar ruta /api/budgets con router de presupuestos

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import colors from "colors";
 import morgan from "morgan";
 import db from "./config/db";
-import colors from "colors";
+import budgetRouter from "./routes/budgetRouter";
+
 
 async function connectDB(){
     try {
@@ -21,5 +23,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 
+//Routes
+app.use("/api/budgets", budgetRouter);
 
 export default app;
