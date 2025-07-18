75db35ce6bec82ed847a84d304f742ac0649e856
Alexz <alexislc330@gmail.com>
2025-07-18 11:54:04 -0600

    feat(database): configurar instancia Sequelize con conexión desde .env

---
+++ backend/src/config/db.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from "sequelize-typescript";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { DATABASE_URL } = process.env;
+
+const sequelize = new Sequelize(DATABASE_URL!, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+});
+
+export default sequelize;
