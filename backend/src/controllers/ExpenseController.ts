3b5a30d1d0b48a68857b36af0926636e58e97ea4
Alexz <alexislc330@gmail.com>
2025-07-19 16:45:09 -0600

    feat(controller): implementar creación de gasto asociado a un presupuesto

--- backend/src/controllers/ExpenseController.ts
+++ backend/src/controllers/ExpenseController.ts
@@ -1,8 +1,21 @@
 import type { Request, Response } from "express";
+import { Expense } from "../models/Expense";
 
 export class ExpenseController {
   static getAll = async (req: Request, res: Response) => {};
-  static create = async (req: Request, res: Response) => {};
+  static create = async (req: Request, res: Response) => {
+    try {
+      const expense = new Expense(req.body);
+      expense.budgetId = Number(req.params.budgetId);
+      
+      await expense.save();
+    
+      res.status(201).json({message: "Gasto creado exitosamente", expense});
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Error al crear el gasto" });
+    }
+  };
   static getById = async (req: Request, res: Response) => {};
   static updateById = async (req: Request, res: Response) => {};
   static deleteById = async (req: Request, res: Response) => {};
