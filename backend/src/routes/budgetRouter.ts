d010b70891e6168763544337ab1b524aa15fbebc
Alexz <alexislc330@gmail.com>
2025-07-19 10:36:17 -0600

    refactor(routes): centralizar validaciones y limpieza de rutas del recurso Budget

--- backend/src/routes/budgetRouter.ts
+++ backend/src/routes/budgetRouter.ts
@@ -2,7 +2,12 @@ import { body, param } from "express-validator";
 import { Router } from "express";
 import { BudgetController } from "../controllers/BudgetController";
 import { handleInputErrors } from "../middleware/validation";
-import { validateBudgetExist, validationBudgetId } from "../middleware/budget";
+import {
+  validateBudgetInput,
+  validateBudgetExist,
+  validationBudgetId,
+} from "../middleware/budget";
+
 const router = Router();
 
 router.param("budgetId", validationBudgetId);
@@ -11,41 +16,16 @@ router.param("budgetId", validateBudgetExist);
 router.get("/", BudgetController.getAll);
 router.post(
   "/",
-  body("name")
-    .notEmpty()
-    .withMessage("El nombre del presupuesto es obligatorio"),
-  body("amount")
-    .notEmpty()
-    .withMessage("El monto del presupuesto es obligatorio")
-
-    .isNumeric()
-    .withMessage("El monto del presupuesto debe ser un número")
-    .custom((value) => value > 0)
-    .withMessage("El monto del presupuesto debe ser mayor a 0"),
+  validateBudgetInput,
   handleInputErrors,
   BudgetController.create
 );
-router.get(
-  "/:budgetId",
-  BudgetController.getById
-);
+router.get("/:budgetId", BudgetController.getById);
 router.put(
   "/:budgetId",
-  body("name")
-    .notEmpty()
-    .withMessage("El nombre del presupuesto es obligatorio"),
-  body("amount")
-    .notEmpty()
-    .withMessage("El monto del presupuesto es obligatorio")
-    .isNumeric()
-    .withMessage("El monto del presupuesto debe ser un número")
-    .custom((value) => value > 0)
-    .withMessage("El monto del presupuesto debe ser mayor a 0"),
+  validateBudgetInput,
   handleInputErrors,
   BudgetController.updateById
 );
-router.delete(
-  "/:budgetId",
-  BudgetController.deleteById
-);
+router.delete("/:budgetId", BudgetController.deleteById);
 export default router;
