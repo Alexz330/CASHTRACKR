83c8b1946f4f18b737da4f3fc98b7b918fb0b366
Alexz <alexislc330@gmail.com>
2025-07-19 09:36:27 -0600

    feat(middleware): validar existencia de presupuesto y extender req con budget

--- backend/src/middleware/budget.ts
+++ backend/src/middleware/budget.ts
@@ -1,5 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { param, validationResult } from "express-validator";
+import Budget from "../models/Budget";
+
+declare global {
+    namespace Express {
+        interface Request {
+            budget?: Budget;
+        }
+    }
+}
 
 export const validationBudgetId = async(req: Request, res: Response, next: NextFunction) => {
     await param("id").isInt().withMessage("Id no valido").custom((value) => value > 0).withMessage("Id no valido").run(req);
@@ -8,4 +17,19 @@ export const validationBudgetId = async(req: Request, res: Response, next: NextF
         return res.status(400).json({ errors: errors.array() });
     }   
     next();
+};
+
+export const validateBudgetExist = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const budget = await Budget.findByPk(req.params.id);
+        if (!budget) {
+            const error = new Error("Presupuesto no encontrado");
+            return res.status(404).json({ message: error.message });
+        }
+        req.budget = budget;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error al obtener el presupuesto" });
+    }
 };
