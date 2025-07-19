be04b07a9b39fc433177ccaba854ae97ef9c1fc7
Alexz <alexislc330@gmail.com>
2025-07-19 15:47:40 -0600

    feat(routes): definir rutas anidadas para gastos dentro de presupuesto

--- backend/src/routes/budgetRouter.ts
+++ backend/src/routes/budgetRouter.ts
@@ -1,6 +1,7 @@
 import { body, param } from "express-validator";
 import { Router } from "express";
 import { BudgetController } from "../controllers/BudgetController";
+import { ExpenseController } from "../controllers/ExpenseController";
 import { handleInputErrors } from "../middleware/validation";
 import {
   validateBudgetInput,
@@ -28,4 +29,12 @@ router.put(
   BudgetController.updateById
 );
 router.delete("/:budgetId", BudgetController.deleteById);
+
+
+/**Routes for expense */
+router.get("/:budgetId/expense", ExpenseController.getAll);
+router.post("/:budgetId/expense", ExpenseController.create);
+router.get("/:budgetId/expense/:expenseId", ExpenseController.getById);
+router.put("/:budgetId/expense/:expenseId", ExpenseController.updateById);
+router.delete("/:budgetId/expense/:expenseId", ExpenseController.deleteById);
 export default router;
