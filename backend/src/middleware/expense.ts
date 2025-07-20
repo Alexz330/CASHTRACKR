bd6161923aee48fb13c23eabf8e119b21749b094
Alexz <alexislc330@gmail.com>
2025-07-20 09:48:20 -0600

    feat(middleware): agregar validador para expenseId en rutas de gastos

--- backend/src/middleware/expense.ts
+++ backend/src/middleware/expense.ts
@@ -1,23 +1,40 @@
 import type { Request, Response, NextFunction } from "express";
-import { validationResult, body} from "express-validator";
+import { validationResult, body, param } from "express-validator";
 
 export const validateExpenseInput = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    await body("name")
-        .notEmpty()
-        .withMessage("El nombre de la gasto es obligatorio")
-        .run(req);
-    await body("amount")
-        .notEmpty()
-        .withMessage("El monto del gasto es obligatorio")
+  await body("name")
+    .notEmpty()
+    .withMessage("El nombre de la gasto es obligatorio")
+    .run(req);
+  await body("amount")
+    .notEmpty()
+    .withMessage("El monto del gasto es obligatorio")
 
-        .isNumeric()
-        .withMessage("El monto del gasto debe ser un número")
-        .custom((value) => value > 0)
-        .withMessage("El monto del gasto debe ser mayor a 0")
-        .run(req);
-    next();
+    .isNumeric()
+    .withMessage("El monto del gasto debe ser un número")
+    .custom((value) => value > 0)
+    .withMessage("El monto del gasto debe ser mayor a 0")
+    .run(req);
+  next();
+};
+
+export const validateExpenseId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  await param("expenseId")
+    .isInt()
+    .custom((value) => value > 0)
+    .withMessage("ID no valido")
+    .run(req);
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  next();
 };
