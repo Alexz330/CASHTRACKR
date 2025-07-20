dda104646c652da48373145863f68288d3a06986
Alexz <alexislc330@gmail.com>
2025-07-20 15:25:45 -0600

    refactor(model): aplicar restricciones not null a campos name y amount de Budget

--- backend/src/models/Budget.ts
+++ backend/src/models/Budget.ts
@@ -1,15 +1,17 @@
-import {Table, Column,Model, DataType, HasMany, BelongsTo, ForeignKey} from "sequelize-typescript";
+import {Table, Column,Model, DataType, HasMany, BelongsTo, ForeignKey, AllowNull} from "sequelize-typescript";
 import { Expense } from "./Expense";
 
 @Table({
     tableName: "budget",
 })
-class Budget extends Model<Budget>{
-  @Column({
+class Budget extends Model{
+  @AllowNull(false)
+  @Column({ 
     type: DataType.STRING(100),
   })
   declare name: string;
 
+  @AllowNull(false)
   @Column({
     type: DataType.DECIMAL,
   })
