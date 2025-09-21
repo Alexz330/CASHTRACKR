3454644389e4fcaabb24cb86a2acd714bb7e8acd
Alexz <alexislc330@gmail.com>
2025-09-21 15:22:47 -0600

    feat(email): añadir plantilla HTML con enlace de confirmación en AuthEmail

--- backend/src/emails/AuthEmail.ts
+++ backend/src/emails/AuthEmail.ts
@@ -1,13 +1,23 @@
+import { transport } from "../config/nodemailer";
+
 type EmailType = {
- name: string;
- email: string;
- token: string; 
-}
-
-
+  name: string;
+  email: string;
+  token: string;
+};
 
 export class AuthEmail {
-    static sendConfirmationEmail = async (user: EmailType) => {
-        console.log(user)
-    }       
+  static sendConfirmationEmail = async (user: EmailType) => {
+    const email = await transport.sendMail({
+      from: "CashTrackr <alexis@cashtrackr.com>",
+      to: user.email,
+      subject: "Confirmacion de cuenta",
+      html: `<p>Hola ${user.name}, has creado una cuenta en CashTrackr</p>
+             <p>Visita el siguiente enlace:</p>
+             <a href="#}">Confirmar cuenta</a>
+           <p> e ingres el codigo de confirmacion: ${user.token}</p>
+           <p>Gracias por usar CashTrackr</p>
+                 `,
+    });
+  };
 }
