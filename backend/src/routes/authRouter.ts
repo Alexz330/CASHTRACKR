08b5075e4ecf707dddfbb5b3490a473b3a144389
Alexz <alexislc330@gmail.com>
2025-09-21 12:42:22 -0600

    feat(auth): añadir validaciones a ruta create-account con express-validator

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,8 +1,15 @@
 import Router from "express";
+import { body } from "express-validator";
 import { AuthController } from "../controllers/AuthController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.post("/create-account", AuthController.createAccount);
+router.post("/create-account",
+    body("name").notEmpty().withMessage("El nombre no puede ir vacio"),
+    body("email").isEmail().withMessage("El correo no es valido"),
+    body("password").isLength({ min: 8 }).withMessage("La contraseña debe tener al menos 8 caracteres"),
+    handleInputErrors,
+    AuthController.createAccount);
 
 export default router;
