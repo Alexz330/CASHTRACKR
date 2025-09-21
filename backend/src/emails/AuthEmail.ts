50ff21ffbb6bad29f7b0948c7b6c99f7f2ce90c6
Alexz <alexislc330@gmail.com>
2025-09-21 15:00:56 -0600

    feat(email): crear clase AuthEmail con método sendConfirmationEmail

---
+++ backend/src/emails/AuthEmail.ts
@@ -0,0 +1,13 @@
+type EmailType = {
+ name: string;
+ email: string;
+ token: string; 
+}
+
+
+
+export class AuthEmail {
+    static sendConfirmationEmail = async (user: EmailType) => {
+        console.log(user)
+    }       
+}
