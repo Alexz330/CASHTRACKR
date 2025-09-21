b9744d27c8d7f35731db5dd01bf226c26b37a96e
Alexz <alexislc330@gmail.com>
2025-09-21 12:51:34 -0600

    feat(auth): implementar creación de cuenta con validación de duplicados en AuthController

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,7 +1,24 @@
 import { Request, Response } from "express";
+import { User } from "../models/User";
 
 export class AuthController{
-    static createAccount(req: Request, res: Response){
-        res.json({message: "create account"})        
+    static async createAccount(req: Request, res: Response){
+        const {name, email, password} = req.body;
+        //Prevenir usuarios duplicados
+        const userExist = await User.findOne({where: {email}});
+
+        if(userExist){
+            const error = new Error("El correo ya esta en uso");
+            error.name = "UserExist";
+            return res.status(400).json({error: error.message});
+        }
+
+        try{
+            const user = await User.create({name, email, password});
+            return res.status(201).json({message: "Cuenta creada exitosamente"});
+        }catch(error){
+            console.log(error);
+            return res.status(500).json({error: error.message})
+        }
     }
 }
