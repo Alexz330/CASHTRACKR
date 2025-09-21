7aaa5be6dcf6cf4fc4a391e872c3aac2cb968079
Alexz <alexislc330@gmail.com>
2025-09-21 15:30:21 -0600

    feat(auth): añadir ruta POST /confirm-account con validación de token

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -5,11 +5,26 @@ import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.post("/create-account",
-    body("name").notEmpty().withMessage("El nombre no puede ir vacio"),
-    body("email").isEmail().withMessage("El correo no es valido"),
-    body("password").isLength({ min: 8 }).withMessage("La contraseña debe tener al menos 8 caracteres"),
-    handleInputErrors,
-    AuthController.createAccount);
+router.post(
+  "/create-account",
+  body("name").notEmpty().withMessage("El nombre no puede ir vacio"),
+  body("email").isEmail().withMessage("El correo no es valido"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("La contraseña debe tener al menos 8 caracteres"),
+  handleInputErrors,
+  AuthController.createAccount
+);
+
+router.post(
+  "/confirm-account",
+  body("token")
+    .isLength({ min: 6 , max: 6})
+    .withMessage("El token debe tener exactamente 6 caracteres")
+    .notEmpty()
+    .withMessage("El token no puede ir vacio"),
+  handleInputErrors,
+  AuthController.confirmAccount
+);
 
 export default router;
